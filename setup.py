14cd2bd772fe7b94e6c229daadbfd47d4d16c2f1
MisterYsLab@gmail.com <brookwood58>
2015-12-16 13:42:27 -0500

    Initial Commit

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'Intended Audience :: Education',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5 ::Only',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Education',
     ],
 )
