8864b0746e0086ce66d545b9f566ed9ba8e88733
MisterYsLab@gmail.com <brookwood58>
2015-12-21 12:48:31 -0500

    Version 1.3 changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='s2aiomulti',
-    version='1.2',
+    version='1.3',
     packages=['s2aios', 's2aior', 's2aiobe'],
     install_requires=['pymata-aio>=2.7',
                       'aiohttp>=0.19.0'],
@@ -28,7 +28,7 @@ setup(
     description='Arduino Multi-Board HTTP Controller and Router',
     keywords=['Firmata', 'Arduino', 'Scratch'],
     classifiers=[
-        'Development Status :: 4- Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Other Environment',
         'Intended Audience :: Education',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
